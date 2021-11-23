c434ac3cc8c18e2cf5546988f3b96e1b5a5dce54
AjinkyaSuryawanshi21 <94887312+AjinkyaSuryawanshi21@users.noreply.github.com>
2021-11-23 12:34:03 +0530

    Update lab3.java

--- project/Lab3.java
+++ project/Lab3.java
@@ -7,7 +7,7 @@ public class Lab3 {
 		System.out.println("Enter a number to continue..");
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextInt();
-		System.out.println("start of main......");
+		System.out.println("start of main...");
 		Runnable simple = ()-> 	  {
 						System.out.println("in run of Simple");
 						try { 			Thread.sleep(3000); 		} catch (InterruptedException e) { 	e.printStackTrace(); 			}
