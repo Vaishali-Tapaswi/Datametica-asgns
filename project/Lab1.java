a7a795e2b8ee928e536ac6a792e65d4e84ca0d61
mssManoj10 <94887205+mssManoj10@users.noreply.github.com>
2021-11-23 12:41:08 +0530

    Added and deleted

--- project/Lab1.java
+++ project/Lab1.java
@@ -4,8 +4,8 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World!!!");
 		        System.out.println("i am sanket..");
+		        System.out.println("Hello Sanket..How are you?");
 			Date d1 = new Date();
 			}
 	}
