7fdf9d759062ec973bf928eab4207cca7c986cbf
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2021-11-23 12:21:13 +0530

    Create Lab1.java

---
+++ project/Lab1.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("..................Hello World................... ");
+			Date d1 = new Date();
+			}
+	}
