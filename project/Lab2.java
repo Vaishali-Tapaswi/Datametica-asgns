3c7e5210c8ed9071e1eb5cf938a6331b952d380f
abhishekagarwal0426 <94887204+abhishekagarwal0426@users.noreply.github.com>
2021-11-23 12:39:07 +0530

    Create Lab2.java
    The args[] will be array of string passed to the main function

--- project/Lab2.java
+++ project/Lab2.java
@@ -2,18 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Lab2 {
-	public static void main(String[] args) throws Exception {
-		System.out.println("Enter a number to continue..");
+public class Lab2{
+	public static void main(String args[]) throws Exception { 
+		System.out.println("Enter a number to continue...!!!!");
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextInt();
-				System.out.println("*****************************");
-
+		
 		System.out.println("For loop initiated");
 		for(int i =0; i< 30000;i++){
 			List<String> list = new ArrayList<>();
 			for(int j =0; j< 30000;j++){
-				list.add(" str " + j);
+				list.add("str" + j);
 			}
 			Thread.sleep(100);
 		}
