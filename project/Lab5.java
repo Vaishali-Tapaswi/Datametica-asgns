c0c988c13c114077754d065e2d6cbbce2351c4f1
nishantinamdar0422 <94887196+nishantinamdar0422@users.noreply.github.com>
2021-11-23 12:39:46 +0530

    Changed list generic from string to integer

--- project/Lab5.java
+++ project/Lab5.java
@@ -6,16 +6,16 @@ import java.util.function.Consumer;
 public class Lab5 {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
 		for(int i =0; i< 30;i++){
-			list.add(" str " + i);
+			list.add(i);
 			}
 	/*	Consumer<String> c1 = (x)->System.out.println(x);
 		list.forEach(c1);
 	*/
 
 //		list.forEach(x->System.out.println(x));
-		list.forEach(System.out println);
+		list.forEach(System.out.println);
 		
 		/*	for (int i = 0; i<list.size();i++){
 			System.out.println(list.get(i));
