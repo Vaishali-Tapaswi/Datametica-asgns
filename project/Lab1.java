1056f18dfaca7ad09c9ecbfee522dfb998296835
manasiBathe23 <94887291+manasiBathe23@users.noreply.github.com>
2021-11-23 12:36:10 +0530

    Update Lab1.java

--- project/Lab1.java
+++ project/Lab1.java
@@ -4,8 +4,8 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("..................Hello World..................... ");
-		        System.out.println("..........Hello Everyone................");
+			System.out.println("..................Hello World!!!!!!!!..................... ");
+		        System.out.println("..........Hello Everyone!!!!!!!!!................");
 			Date d1 = new Date();
 			}
 	}
